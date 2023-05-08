ed4c80f19e644514dc0675eb96aca2fdfbd3935f
jubaer919 <60255197+jubaer919@users.noreply.github.com>
2023-05-08 18:35:30 +0600

    fix some linter issues

--- modules/datetime.js
+++ modules/datetime.js
@@ -2,7 +2,6 @@ export default function dateAndTime() {
   // Get the current local time and date
   const now = new Date();
 
-  // Format the time and date
  const options = {
     weekday: 'long',
     year: 'numeric',
@@ -15,7 +14,6 @@ export default function dateAndTime() {
   };
   const formattedTime = now.toLocaleDateString('en-US', options);
 
-  // Render the time and date on the webpage
   const localTimeElement = document.querySelector('.date-time');
   localTimeElement.innerHTML = formattedTime;
 }
